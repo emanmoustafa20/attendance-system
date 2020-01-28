38bfd9228f55770447270ac8e02420d1660f8aad
Eman Mostafa <you@example.com>
2020-01-28 22:51:47 +0200

    RFID Code

--- data_base.py
+++ data_base.py
@@ -1,7 +1,13 @@
 from firebase import firebase
+from firebase_admin import db
+import firebase_admin
+from firebase_admin import credentials
 firebase=firebase.FirebaseApplication('https://attendancesystem-6c7c0.firebaseio.com/',None)
 
 
+get_data= firebase.get ('Students',str('Asmaa'))
+control=firebase.put('Students',str('Asmaa'), get_data+1)
+get_data= firebase.get ('Students',str('Asmaa'))
+print (get_data)
 
-control=firebase.post('presentStudents', 2 )
 
